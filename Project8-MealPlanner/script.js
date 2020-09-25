b7bb222b0a1a3b0e80a40231331582dc38cee202
Stewart Newnham <git@newnham.com>
2020-09-25 10:09:13 +0800

    Added CommentsW

--- Project8-MealPlanner/script.js
+++ Project8-MealPlanner/script.js
@@ -1,25 +1,28 @@
 // Select Elements
-// ---------------
-const search = document.getElementById("search");
-const submit = document.getElementById("submit");
-const random = document.getElementById("random");
-const mealsEl = document.getElementById("meals");
-const resultHeading = document.getElementById("result-heading");
-const single_mealEl = document.getElementById("single-meal");
+// ===============
+const search = document.getElementById("search"); // Form Input
+const submit = document.getElementById("submit"); // Submit Button
+const random = document.getElementById("random"); // Random Button
+const resultHeading = document.getElementById("result-heading"); // Dynamic Heading for Meal
+const mealsEl = document.getElementById("meals"); // Dynamic Div for Grid Display of Meals
+const single_mealEl = document.getElementById("single-meal"); // Dynamic Div for Single Meal
 
 // Event Listeners
-// ---------------
-submit.addEventListener("submit", searchMeal);
-random.addEventListener("click", getRandomMeal);
+// ===============
+submit.addEventListener("submit", searchMeal); // Submit Button
+random.addEventListener("click", getRandomMeal); // Random Button
 
+// LISTENER to Select Meal from Grid of Meals
 mealsEl.addEventListener("click", (e) => {
+  // clicking meal in grid selects multiple divs. Need to select div with class meal-info.
   const mealInfo = e.path.find((item) => {
     if (item.classList) {
       return item.classList.contains("meal-info");
     } else {
       return false;
     }
-  });
+  }); // returns div with class "meal-Info" and "data-mealid" that was generated dynamically by searchMeal()
+  console.log(mealInfo);
   if (mealInfo) {
     const mealID = mealInfo.getAttribute("data-mealid");
     getMealById(mealID);
@@ -27,30 +30,28 @@ mealsEl.addEventListener("click", (e) => {
 });
 
 // Functions
-// ---------
+// =========
 
-// SearchMeal() - search API with fetch
-// ------------
+// SearchMeal()
+// -------------
+// 1. search  by food item using fetch API
+// 2. populate grid with images of all meals and its name using map()
 function searchMeal(e) {
   e.preventDefault(); // dont submit to a file
 
-  // Clear single meal
-  single_mealEl.innerHTML = "";
+  single_mealEl.innerHTML = ""; // Clear single meal dynamically
 
-  // Ger search Term
-  const term = search.value;
+  const term = search.value; // Get the user's food item search input
 
-  // check for empty
   if (term.trim()) {
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
       .then((res) => res.json())
       .then((data) => {
-        // console.log(data);
-        resultHeading.innerHTML = `<h2>Search Results for: '${term}'</h2>`;
-
         if (data.meals == null) {
           resultHeading.innerHTML = `<p>There are no results for '${term}'</p>`;
+          mealsEl.innerHTML = "";
         } else {
+          resultHeading.innerHTML = `<h2>Search Results for: '${term}'</h2>`;
           mealsEl.innerHTML = data.meals
             .map(
               (meal) => `
@@ -72,7 +73,7 @@ function searchMeal(e) {
   }
 }
 
-// getMealById()
+// getMealById() - API search for food Item given API
 // -------------
 function getMealById(mealID) {
   fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
@@ -84,6 +85,13 @@ function getMealById(mealID) {
     });
 }
 
+// addMealToDOM(meal)
+// ------------------
+// 1. populate ar array with meal ingredients and its portion
+// 2. Dynamically fill single-meal div
+//          a. large photo
+//          b. recipe details
+//          c. ingredients using Map()
 function addMealToDOM(meal) {
   const ingredients = [];
   for (let i = 1; i <= 20; i++) {
@@ -95,7 +103,6 @@ function addMealToDOM(meal) {
       break;
     }
   }
-
   single_mealEl.innerHTML = `
   <div class="single-meal">
     <h1>${meal.strMeal}</h1>
@@ -115,6 +122,8 @@ function addMealToDOM(meal) {
   `;
 }
 
+// getRandomMeal() - gets a random meal from the API and populates DOM with addMealToDOM()
+// ---------------
 function getRandomMeal() {
   // Clear old data
   mealsEl.innerHTML = "";
