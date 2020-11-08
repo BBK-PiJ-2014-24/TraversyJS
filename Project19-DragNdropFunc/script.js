8939da85295a1c1e076d91823b3ed37eed3d44ff
Stewart Newnham <git@newnham.com>
2020-11-08 22:47:03 +0800

    added createList()

---
+++ Project19-DragNdropFunc/script.js
@@ -0,0 +1,52 @@
+// DOM ELEMENTS
+// ============
+const draggable_list = document.getElementById('draggable-list');
+const check = document.getElementById('check');
+
+// GLOBAL VARIABLES
+// ================
+const richestPeople = [
+    'Jeff Bezos',
+    'Bill Gates',
+    'Warren Buffett',
+    'Bernard Arnault',
+    'Carlos Slim Helu',
+    'Amancio Ortega',
+    'Larry Ellison',
+    'Mark Zuckerberg',
+    'Michael Bloomberg',
+    'Larry Page'
+  ];
+  
+  const listItems = [];
+
+  let dragStartIndex;
+
+// FUNCTIONS
+// =========
+// createList() 
+// ------------
+// N.b. [...richPeope] is a copy
+// map() - creates an object for each person with keys 'value' and 'sort' 
+// sort() - sort random array
+// map() - map back to simple array 
+function createList(){
+    [...richestPeople]
+    .map(a => ({value: a, sort: Math.random()}))  
+    .sort((a,b)=>(a.sort - b.sort))
+    .map(a => a.value)
+    .forEach((person, index)=>{
+        const listItem = document.createElement('li');
+        listItem.setAttribute('data-index', index);
+        listItem.innerHTML =`
+            <span class='number'>${index+1}</span>
+            <div class='draggable' draggable='true'>
+                    <p class='person-name'>${person}</p>
+                    <i class='fas fa-grip-lines'></i>
+            </div>
+            `;
+        listItems.push(listItem);    
+        draggable_list.appendChild(listItem);
+        });
+}
+createList();
