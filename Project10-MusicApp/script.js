1d03a6eed52a7a819c07e47944c3b74edf69fd94
Stewart Newnham <git@newnham.com>
2020-11-16 22:25:32 +0800

    added progress bar functionality

--- Project10-MusicApp/script.js
+++ Project10-MusicApp/script.js
@@ -59,6 +59,18 @@ function nextSong(){
     playSong();
 }
 
+function updateProgress(e){
+    const {duration, currentTime} = e.srcElement;
+    const progressPercent = (currentTime/duration)*100;
+    progress.style.width = `${progressPercent}%`;
+}
+
+function setProgress(e){
+    const width = this.clientWidth;
+    const clickX = e.offsetX;
+    const duration = audio.duration;
+    audio.currentTime = (clickX / width)*duration;
+}
 
 // EVENT LISTENERS
 // ===============
@@ -73,4 +85,6 @@ playBtn.addEventListener('click', ()=>{
 
 prevBtn.addEventListener('click',prevSong);
 nextBtn.addEventListener('click',nextSong);
-
+audio.addEventListener('timeupdate', updateProgress); //update progress bar
+progressContainer.addEventListener('click', setProgress); // click to jump around in song
+audio.addEventListener('ended', nextSong); // autoplay next song
